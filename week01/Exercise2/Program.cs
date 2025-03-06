e54769f1f02906ca7dc0e0f52c0711621c663e72
kelvincool0506 <kelvincoolonuoha@gmail.com>
2025-03-06 17:41:07 +0000

    finished exercise3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -15,16 +15,6 @@ class Program
             letter = "A";
         }
         else if (percent >= 80)
-        {
-            letter = "B";
-        }
-        else if (percent >= 70)
-        {
-            letter = "C";
-        }
-        else if (percent >= 60)
-        {
-            letter = "D";
         }
         else 
         {
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,42 @@
-using System;
-
-class Program
+class Program 
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
-}
+        // For Parts 1 and 2, where the user specified the number...
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int guess = 0; 
+        int attempts = 0;
+
+        Console.WriteLine(" Welcome to the Guess My Number game.");
+        Console.Write("What is the magic number? ");
+
+        // Use a while loop here...
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("what is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            attempts++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("lower");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations! you Guessed right in  {attempts} attempts.");
+            }
+        }
+    } // This closes the Main method
+
+} // This closes the Program class
